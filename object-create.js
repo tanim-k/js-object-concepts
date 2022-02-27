5d0c8ab5e911f113bc2a180458b80aab7bb7a450
tanim-k <tanimkhan877@gmail.com>
2022-02-27 15:28:41 +0600

    creating object new from parent object using class

--- object-create.js
+++ object-create.js
@@ -3,3 +3,18 @@ const student = {name:'sakib al', job:'studying', age:30};
 // 2. constructor
 const person = new Object()
 console.log(person);
+// 3.
+const human = Object.create(student);
+const humanNull = Object.create(null);
+console.log(human.job);
+console.log(humanNull);
+
+// try to make a class 
+class People{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+const peop = new People('bosir kha', 76);
+console.log(peop);
