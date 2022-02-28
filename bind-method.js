2e54a9343e935713c7c3449f823907bbbf6a54cc
tanim-k <tanimkhan877@gmail.com>
2022-02-28 16:24:24 +0600

    bind-method practice

--- bind-method.js
+++ bind-method.js
@@ -1,17 +1,38 @@
-const student = {
+const kib = {
     id: 101,
     name: 'js kolu armagita',
     money: 5000,
-    major: 'mathematics',
-    isRich: false,
-    subject: ['english','economics', 'math101'],
-    bestFriend: {
-        name: 'mundu',
-        major: 'mathematics',
-    },
-    treatDey: function(expense, boksis) {
-        this.money = this.money-expense-boksis;
+    name: 'rj kibria',
+    treatDey: function(expense) {
+        this.money = this.money-expense;
+        console.log(this)
         return this.money;
     }
     
 };
+const heroBalam = {
+    id: 102,
+    money:6000,
+    name: 'hero balam'
+}
+const normalGolam = {
+    id: 103,
+    money: 8000,
+    name: 'golam ali'
+}
+const amitSaha = {
+    id: 104,
+    money: 10000,
+    name: 'amit shah'
+}
+kib.treatDey(500);
+const heroTreatDey = kib.treatDey.bind(heroBalam)
+heroTreatDey(500)
+heroTreatDey(1500)
+heroTreatDey(300)
+kib.treatDey(400)
+
+const normalTreatDey = kib.treatDey.bind(normalGolam);
+normalTreatDey(500);
+const amitTreatDey = kib.treatDey.bind(amitSaha);
+amitTreatDey(5000);
