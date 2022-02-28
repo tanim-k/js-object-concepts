218fb6fb2a54aa9be3b5a62000d7787165568f74
tanim-k <tanimkhan877@gmail.com>
2022-02-28 15:03:39 +0600

    compare function

--- compare-object.js
+++ compare-object.js
@@ -1,8 +1,8 @@
-const first ={a: 1, b: 4};
-const second ={a: 1, b: 4};
+const first ={a: 1, b: 2};
+const second ={b: 2, a: 1};
 const third = first;
 
-if(first === third){
+if(first === second){
     console.log('objects are equal');
 }
 else{
@@ -11,5 +11,19 @@ else{
 console.log(JSON.stringify(first));
 console.log(JSON.stringify(second));
 if(JSON.stringify(first) === JSON.stringify(second)){
-
+    console.log('objects are equal')
+};
+// make a function to compare two elements 
+function compareObjects(obj1, obj2){
+    if(Object.keys(obj1).length !== Object.keys(obj2).length ) {
+        return false;
+    }
+    for(const prop in obj1) {
+        if(obj1[prop] !== obj2[prop]) {
+            return false;
+        }
+    }
+    return true;
 }
+const isEqual = compareObjects(first, second);
+console.log(isEqual);
