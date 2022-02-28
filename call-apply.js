f892e8f2b3343ccf8fa35afcd590f6d3d3031897
tanim-k <tanimkhan877@gmail.com>
2022-02-28 19:03:12 +0600

    call fn

--- call-apply.js
+++ call-apply.js
@@ -1,11 +1,10 @@
 const kib = {
     id: 101,
-    name: 'js kolu armagita',
     money: 5000,
     name: 'rj kibria',
     treatDey: function(expense) {
         this.money = this.money-expense;
-        console.log(this)
+        console.log('hi',this)
         return this.money;
     }
     
@@ -20,22 +19,5 @@ const normalGolam = {
     money: 8000,
     name: 'golam ali'
 }
-const amitSaha = {
-    id: 104,
-    money: 10000,
-    name: 'amit shah'
-}
-kib.treatDey(500);
-// const heroTreatDey = kib.treatDey.bind(heroBalam)
-// heroTreatDey(500)
-// heroTreatDey(1500)
-// heroTreatDey(300)
-// kib.treatDey(400)
-
-// const normalTreatDey = kib.treatDey.bind(normalGolam);
-// normalTreatDey(500);
-// const amitTreatDey = kib.treatDey.bind(amitSaha);
-// amitTreatDey(5000);
-
 // call function 
 kib.treatDey.call(heroBalam, 500)
